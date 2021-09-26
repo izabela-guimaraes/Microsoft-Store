a8c039ed237f35e29aa2a2105f39212770af90b9
Izabela Guimararães <izabela.guimaraes.nascimento@gmail.com>
2021-09-26 18:40:14 -0300

    finalized site

---
+++ app.js
@@ -0,0 +1,13 @@
+const menulist = document.getElementById("menu-list")
+
+menulist.style.maxHeight = "0px"
+
+function showmenu(){
+
+    if(menulist.style.maxHeight == "0px"){
+        menulist.style.maxHeight = "120px"
+    }
+    else{
+        menulist.style.maxHeight = "0px"
+    } 
+}
